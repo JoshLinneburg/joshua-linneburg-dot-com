3230a1bbaabb63399c18e7733c7b53375cbaa54b
Josh <joshualinneburg@gmail.com>
2021-05-26 22:43:14 -0400

    Updated api.py to have shell context for debugging

--- api.py
+++ api.py
@@ -1,8 +1,15 @@
 from flask.cli import FlaskGroup
-from app import create_app
+from app import create_app, db
+from app.models import User, Post, Comment, Tag, PostTag
 
 app = create_app()
 cli = FlaskGroup(app)
 
 if __name__ == "__main__":
     cli()
+
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Post': Post, 'Tag': Tag,
+            'Comment': Comment, 'PostTag': PostTag}
