4e4346edb4b1729db4ca185a6072a0d89fc98a93
Josh <joshualinneburg@gmail.com>
2021-05-26 22:23:53 -0400

    Added models, fixed boilerplate code to run basic 'Hello World'

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from errors.handlers import jwt
 from flask_migrate import Migrate
 from flask_cors import CORS
+from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 from config import Config
@@ -9,6 +10,7 @@ from flasgger import Swagger
 
 db = SQLAlchemy()
 ma = Marshmallow()
+login = LoginManager()
 migrate = Migrate()
 cors = CORS()
 swag = Swagger()
@@ -19,12 +21,13 @@ def create_app(config_class=Config):
     app.config.from_object(config_class)
     db.init_app(app=app)
     ma.init_app(app=app)
+    login.init_app(app=app)
     migrate.init_app(app=app, db=db)
     jwt.init_app(app=app)
     cors.init_app(app=app)
     swag.init_app(app=app)
 
-    from home.routes import home_bp
+    from .home.routes import home_bp
     app.register_blueprint(home_bp)
 
     return app
---
+++ app/models.py
@@ -0,0 +1,66 @@
+import base64
+import json
+import jwt
+import os
+
+from app import db, login
+from datetime import datetime, timedelta
+from flask import current_app, url_for
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    public_id = db.Column(db.String(128), unique=True)
+    profile_picture_path = db.Column(db.String(128))
+    email = db.Column(db.String(50), unique=True)
+    phone_nbr = db.Column(db.String(15), unique=True)
+    password_hash = db.Column(db.String(128), nullable=False)
+    posts = db.relationship("Post", backref="author", lazy="dynamic")
+    comments = db.relationship("Comment", backref="author", lazy="dynamic")
+    created_datetime = db.Column(db.DateTime, default=datetime.utcnow)
+    modified_datetime = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+
+    def set_password(self, password):
+        """
+        Sets the password of the User to the password passed in.
+        Args:
+            password: str
+                A str containing the User's new password.
+        Returns:
+            None.
+        """
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        """
+        Checks the hash of a User's password against the hash of a password they've
+        provided for authentication.
+        Args:
+            password: str
+                A str containing the password the User wants to authenticate with.
+        Returns:
+            result: bool
+                Whether the password the User provided matches the
+                password associated with their account.
+        """
+        result = check_password_hash(self.password_hash, password)
+        return result
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    public_id = db.Column(db.String(128), unique=True)
+    body = db.Column(db.Text)
+    comments = db.relationship("Comment", backref="post", lazy="dynamic")
+    created_datetime = db.Column(db.DateTime, default=datetime.utcnow)
+    modified_datetime = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+
+
+class Comment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    public_id = db.Column(db.String(128), unique=True)
+    body = db.Column(db.Text)
+    created_datetime = db.Column(db.DateTime, default=datetime.utcnow)
+    modified_datetime = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
