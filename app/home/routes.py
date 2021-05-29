13a5ed6f686e8309de20955360f37fb6026e7f19
Josh <joshualinneburg@gmail.com>
2021-05-28 20:49:05 -0400

    Test commit

--- app/home/routes.py
+++ app/home/routes.py
@@ -6,4 +6,4 @@ home_bp = Blueprint("home_bp", __name__)
 @home_bp.route("/")
 @home_bp.route("/home")
 def homepage():
-    return "Hello world!"
+    return "Should have used CloudFlare. You hate to see it, folks. :("
